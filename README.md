c5b87ae69efb75ff781fa20fa35ea5d97aa3dd14
mike011 <mike_ch_1@hotmail.com>
2024-02-21 21:56:02 -0500

    First commit with tab bar setup

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # StravaUIKitClone
 A simple duplication effort of what the current Strava app looks like
+
+Time Spent: 1 hour
---
+++ StravaUIKitClone/StravaUIKitClone/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  StravaUIKitClone
+//
+//  Created by Michael Charland on 2024-02-21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
